d91c0b430e8d1dcdde4626da1e02fa4960f9473c
StrHiugh <116786621+StrHiugh@users.noreply.github.com>
2024-09-17 18:25:08 +0800

    sidebar funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css'
-import {Card, CardHeader, CardBody, CardFooter, Avatar, Button} from "@nextui-org/react";
-import React from "react";
-import BlogLayout from "./components/blog"
-import Topbar from './components/Topbar';
+import Sidebar, { SidebarItem } from "./components/global/Sidebar.jsx";
+import { BarChart, Cpu, LayoutDashboard, Route, SquareChartGantt, SquareDashedKanban } from "lucide-react";
 
 function App() {
-    const [isFollowed, setIsFollowed] = React.useState(false);
-
     return (
-        <>
-            <div className="App">
-                <Topbar />  {/* Barra de navegación */}
-                <BlogLayout/>
-            </div>
-        </>
+        <main className="App flex">
+            {/* Sidebar */}
+            <Sidebar>
+                <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" alert />
+                <hr className="my-3" />
+                <SidebarItem icon={<BarChart size={20} />} text="Proceso" active />
+                <SidebarItem icon={<Cpu size={20} />} text="Equipo" />
+                <SidebarItem icon={<Route size={20} />} text="Etapa" />
+                <SidebarItem icon={<SquareChartGantt size={20} />} text="Producto" />
+                <SidebarItem icon={<SquareDashedKanban size={20} />} text="Sensores" />
+            </Sidebar>
+
+            {/* Main content */}
+            <section className="ml-32 p-6 transition-margin duration-300">
+                {/* Aquí irá el contenido principal de tu aplicación */}
+                <h1>Bienvenido al Dashboard</h1>
+            </section>
+        </main>
     );
 }
 
-export default App
+export default App;
